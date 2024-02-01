0633d069028602a55f403a968bb2fc0476ea3ee8
emna <emna.jridi@esen.tn>
2024-02-01 13:41:50 +0100

    de 1 à 7 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(stringToRepeat, num) {
+ let st = stringToRepeat; 
+for (let i = 1; i<  num ; i++){
+st+=stringToRepeat; 
+}
+return st ; 
 };
 
 // Do not edit below this line
